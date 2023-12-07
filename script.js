dc9ddef5a80d3368a39f21319bf2f97a0bd0dadc
Kulpunai04 <150809938+Kulpunai04@users.noreply.github.com>
2023-12-07 17:54:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+/*
+let num = ['a', 'b', 'c', 'd'];
+alert(num[0] + num[1]);
+alert(num[2] + num[3]);
+*/
 
 
-let i;
-for (let i=10; i>0; i--){
-    console.log(i);
+
+
+
+/*
+let numbers= [1, 2, 5, 6, 88,5];
+let one = 0;
+for (let i = 0; i < numbers.length; i++) {
+  one += numbers[i];
 }
-
-
-
+console.log(one);
+*/
 
